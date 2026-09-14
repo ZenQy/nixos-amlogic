41170d2788276fb38ce529cf855484497c9d6874
ZenQy <zenqy.qin@gmail.com>
2026-09-14 20:06:09 +0800

    更换 u-boot 文件 明确复制文件的文件名

--- devices/tx3/default.nix
+++ devices/tx3/default.nix
@@ -6,26 +6,29 @@
     (modulesPath + "/installer/sd-card/sd-image.nix")
   ];
 
-  # 3. 自定义 SD 卡镜像行为
   sdImage = {
     populateFirmwareCommands = "";
     populateRootCommands = ''
       mkdir -p ./files/boot
-      cp ${./emmc_autoscript} ./files/boot/emmc_autoscript
-      cp ${./uboot} ./files/boot/uboot
+      cp ${../emmc_autoscript} ./files/boot/emmc_autoscript
+      cp ${../u-boot-tx3-qz.bin} ./files/boot/uboot
       ${config.boot.loader.generic-extlinux-compatible.populateCmd} -c ${config.system.build.toplevel} -d ./files/boot
     '';
-    postBuildCommands = ''
-      dd if=${./uboot} of=$img conv=fsync,notrunc bs=1 count=444
-      dd if=${./uboot} of=$img conv=fsync,notrunc bs=512 skip=1 seek=1
-    '';
+    postBuildCommands =
+      let
+        bootloader = ../x96maxplus-u-boot.bin.sd.bin;
+      in
+      ''
+        dd if=${bootloader} of=$img conv=fsync,notrunc bs=1 count=444
+        dd if=${bootloader} of=$img conv=fsync,notrunc bs=512 skip=1 seek=1
+      '';
   };
   image.baseName = "tx3";
 
   hardware.deviceTree = {
     enable = true;
-    name = "amlogic/meson-sm1-x96-air-gbit.dtb";
-    filter = "*x96-air*.dtb";
+    name = "meson-sm1-tx3-qz.dtb";
+    dtbSource = ./dtbs;
   };
 
   systemd.network.networks.eth0 = {
